3c1ab40252aa265496979564326106620196eda6
ma4dos <dosuzhaya.mari@bk.ru>
2026-04-05 22:12:20 +0300

    Update alg.cpp
    Досужая Мария 25-ист-4-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,26 @@
 // Copyright 2021 NNTU-CS
+#include <unordered_map>
+
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    std::unordered_map<int, int> freq;
+    for (int i = 0; i < len; ++i) {
+        freq[arr[i]]++;
+    }
+    int result = 0;
+    for (auto &entry : freq) {
+        int a = entry.first;
+        int b = value - a;
+        if (a > b) continue;
+        if (a == b) {
+            int c = entry.second;
+            result += c * (c - 1) / 2;
+        } else {
+            auto it = freq.find(b);
+            if (it != freq.end()) {
+                result += entry.second * it->second;
+            }
+        }
+    }
+    return result;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
-}
-int countPairs3(int *arr, int len, int value) {
-  return 0;
-}
+  
