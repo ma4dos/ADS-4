2e80f494aa9f6d316ada6ebdb9a1738598592f3e
ma4dos <dosuzhaya.mari@bk.ru>
2026-04-05 22:29:20 +0300

    Update alg.cpp
    Досужая Мария 25-ист-4-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,74 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  return 0;
+#include <unordered_map>
+#include <algorithm>
+
+int countPairs1(int* arr, int len, int value) {
+    int count = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = i + 1; j < len; ++j) {
+            if (arr[i] + arr[j] == value) {
+                ++count;
+            }
+        }
+    }
+    return count;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
+
+int countPairs2(int* arr, int len, int value) {
+    std::sort(arr, arr + len);
+    int count = 0;
+    int left = 0;
+    int right = len - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == value) {
+            if (arr[left] == arr[right]) {
+                int n = right - left + 1;
+                count += n * (n - 1) / 2;
+                break;
+            } else {
+                int leftVal = arr[left];
+                int rightVal = arr[right];
+                int leftCount = 0;
+                int rightCount = 0;
+                while (left <= right && arr[left] == leftVal) {
+                    ++leftCount;
+                    ++left;
+                }
+                while (right >= left && arr[right] == rightVal) {
+                    ++rightCount;
+                    --right;
+                }
+                count += leftCount * rightCount;
+            }
+        } else if (sum < value) {
+            ++left;
+        } else {
+            --right;
+        }
+    }
+    return count;
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
+
+int countPairs3(int* arr, int len, int value) {
+    std::unordered_map<int, int> freq;
+    for (int i = 0; i < len; ++i) {
+        freq[arr[i]]++;
+    }
+    int result = 0;
+    for (const auto& entry : freq) {
+        int a = entry.first;
+        int b = value - a;
+        if (a > b) continue;
+        if (a == b) {
+            int c = entry.second;
+            result += c * (c - 1) / 2;
+        } else {
+            auto it = freq.find(b);
+            if (it != freq.end()) {
+                result += entry.second * it->second;
+            }
+        }
+    }
+    return result;
 }
